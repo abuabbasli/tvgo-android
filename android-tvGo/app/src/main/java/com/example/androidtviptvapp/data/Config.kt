55e81a1dc4e395d44e76d4a3a414aa6a09d449e3
abu <ebuabbasli95@gmail.com>
2026-01-24 23:45:26 +0100

    final fixj1uZn7ci

--- android-tvGo/app/src/main/java/com/example/androidtviptvapp/data/Config.kt
+++ android-tvGo/app/src/main/java/com/example/androidtviptvapp/data/Config.kt
@@ -13,14 +13,14 @@ object AppConfig {
     // Local backend - use your computer's IP for physical device
     // For emulator use: 10.0.2.2
     // For physical device use your computer IP (currently: 192.168.1.76)
-    // const val BASE_URL = "http://10.0.2.2:8000/"
-    // const val IMAGE_BASE_URL = "http://10.0.2.2:8000"
-    // const val SERVER_IP = "10.0.2.2"
+    const val BASE_URL = "http://10.0.2.2:8000/"
+    const val IMAGE_BASE_URL = "http://10.0.2.2:8000"
+    const val SERVER_IP = "10.0.2.2"
 
     // ===========================================
     // PRODUCTION - Lambda URL (comment out when testing locally)
     // ===========================================
-    const val BASE_URL = "https://hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws/"
-    const val IMAGE_BASE_URL = "https://hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws"
-    const val SERVER_IP = "hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws"
+    // const val BASE_URL = "https://hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws/"
+    // const val IMAGE_BASE_URL = "https://hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws"
+    // const val SERVER_IP = "hsbcasafqma6eflzbulquhxflu0stbuw.lambda-url.eu-central-1.on.aws"
 }
